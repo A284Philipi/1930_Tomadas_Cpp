b67d459d1cf4a0f9780361e2366b630f0b18984a
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 19:09:08 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    a = (a + b + c + d) - 3;
+    cout << a <<endl;
+    return 0;
+}
